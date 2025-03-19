07675670a3bee04fd1ac512a78c3bb2e81cc7410
duongthuan0405 <duongthuan0405@gmail.com>
2025-03-19 16:37:44 +0700

    Initialization QuanLyHocSinh solution

---
+++ QuanLyHocSinh/DAL/TestDAL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class TestDAL
+    {
+        public static string Method()
+        {
+            return "DAL " + DTO.TestDTO.Method();
+        }
+    }
+}
