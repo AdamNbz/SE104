cdfdc48dffaa9d800131b58489b2e4e4d30007ba
nguyen thi <thinguyen12112005@gmail.com>
2025-03-26 17:35:59 +0700

    Change how error is checked

--- QuanLyHocSinh/BLL/HocSinhBLL.cs
+++ QuanLyHocSinh/BLL/HocSinhBLL.cs
@@ -6,6 +6,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace BLL;
 
@@ -20,14 +21,42 @@ public static class HocSinhBLL
 
     public static bool TiepNhanHocSinh(HocSinh hs)
     {
-        if (!KiemTraHopLeVoiHocSinh(hs))
+        if (hs.Email == "")
+        {
+            MessageBox.Show("EMAIL KHÔNG HỢP LỆ", "TIẾP NHẬN THẤT BẠI", MessageBoxButton.OK, MessageBoxImage.Error);
             return false;
+        }
+        else if (hs.GioiTinh == "")
+        {
+            MessageBox.Show("GIOITINH KHÔNG HỢP LỆ", "TIẾP NHẬN THẤT BẠI", MessageBoxButton.OK, MessageBoxImage.Error);
+            return false;
+        }
+        else if (hs.DiaChi == "")
+        {
+            MessageBox.Show("DIACHI KHÔNG HỢP LỆ", "TIẾP NHẬN THẤT BẠI", MessageBoxButton.OK, MessageBoxImage.Error);
+            return false;
+        }
+        else if (hs.HoTen == "")
+        {
+            MessageBox.Show("HOTEN KHÔNG HỢP LỆ", "TIẾP NHẬN THẤT BẠI", MessageBoxButton.OK, MessageBoxImage.Error);
+            return false;
+        }
+        else if (hs.MaHS == "")
+        {
+            MessageBox.Show("MAHS KHÔNG HỢP LỆ", "TIẾP NHẬN THẤT BẠI", MessageBoxButton.OK, MessageBoxImage.Error);
+            return false;
+        }
+        else if (!KiemTraTuoiHopLeVoiHocSinh(hs))
+        {
+            MessageBox.Show("Tuoi KHÔNG HỢP LỆ", "TIẾP NHẬN THẤT BẠI", MessageBoxButton.OK, MessageBoxImage.Error);
+            return false;
+        }
 
         if (HocSinhDAL.TiepNhanHocSinh(hs) == 1)
             return true;
         return false;
     }
-    private static bool KiemTraHopLeVoiHocSinh(HocSinh hs)
+    private static bool KiemTraTuoiHopLeVoiHocSinh(HocSinh hs)
     {
         int tuoiToiThieu = ThamSoDAL.LayTuoiToiThieu();
         int tuoiToiDa = ThamSoDAL.LayTuoiToiDa();
